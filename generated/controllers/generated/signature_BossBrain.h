28efb856dfde7084762ff1a209c4a2173669583d
GitHub git update Action <action@github.com>
2025-05-15 00:48:43 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_BossBrain.h
+++ generated/controllers/generated/signature_BossBrain.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3460673092
-#define TS_SIGNATURE "rusEFI main.2025.05.14.BossBrain.3460673092"
+#define SIGNATURE_HASH 4097490908
+#define TS_SIGNATURE "rusEFI main.2025.05.15.BossBrain.4097490908"
