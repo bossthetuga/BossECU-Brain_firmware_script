f845d2a374cad29e14be6eba973a0078f5241add
BossTheTuga <theboss720p@gmail.com>
2025-05-20 15:08:46 +0100

    Update board.mk

--- board.mk
+++ board.mk
@@ -6,6 +6,9 @@ BOARDINC += $(BOARD_DIR)/generated/controllers/generated
 # defines SHORT_BOARD_NAME
 include $(BOARD_DIR)/meta-info.env
 
+# assign critical LED to a non-existent pin
+DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::G11
+
 # assign critical LED to a non-existent pin if you do not have it on your board
 # good old PD14 is still the default value
 # DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
