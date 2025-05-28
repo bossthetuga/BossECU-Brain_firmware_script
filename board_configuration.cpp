b9ba4f404744ee380d9fd14f02c4652455b3daf1
BossTheTuga <theboss720p@gmail.com>
2025-05-28 14:29:23 +0100

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -5,15 +5,6 @@
  */
 
 #include "pch.h"
-#include "uart_uart.h"
-extern Logging uart1;
-
-void initBoard() {
-    // Other initialization...
-
-    // USART1: PA9 (TX), PA10 (RX)
-    uart1.init(UART_DEVICE_USART1, 115200);
-}
 
 static void setInjectorPins() {
 	engineConfiguration->injectionPins[0] = Gpio::F13;
